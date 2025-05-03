792fd4ab55aab302c82f0f0b8b2ea5f553ce2a19
▓▒░ ♔ Daniel ♔ ░▒▓ <563975+easierbycode@users.noreply.github.com>
2025-05-03 20:07:55 +0000

    add nineslice container

--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -20,16 +20,21 @@ export default class Level extends Phaser.Scene {
 
 	editorCreate(): void {
 
-		// fufuSuperDino
-		const fufuSuperDino = this.add.image(400, 235, "FufuSuperDino") as Phaser.GameObjects.Image & { body: Phaser.Physics.Arcade.Body };
-		this.physics.add.existing(fufuSuperDino, false);
-		fufuSuperDino.body.velocity.x = 100;
-		fufuSuperDino.body.velocity.y = 100;
-		fufuSuperDino.body.bounce.x = 1;
-		fufuSuperDino.body.bounce.y = 1;
-		fufuSuperDino.body.collideWorldBounds = true;
-		fufuSuperDino.body.setOffset(31, 31);
-		fufuSuperDino.body.setCircle(91);
+		// container_1
+		const container_1 = this.add.container(0, 0);
+
+		// nineslice_1
+		const nineslice_1 = this.add.nineslice(100, 100, "panel", undefined, 600, 400, 32, 32, 32, 32);
+		nineslice_1.setOrigin(0, 0);
+		container_1.add(nineslice_1);
+
+		const boundsX = nineslice_1.x;
+		const boundsY = nineslice_1.y;
+		const boundsWidth = nineslice_1.width;
+		const boundsHeight = nineslice_1.height;
+
+		this.physics.world.setBounds(boundsX, boundsY, boundsWidth, boundsHeight);
+
 
 		// barbarian
 		const barbarian = this.add.image(220, 192, "barbarian") as Phaser.GameObjects.Image & { body: Phaser.Physics.Arcade.Body };
@@ -41,9 +46,25 @@ export default class Level extends Phaser.Scene {
 		barbarian.body.collideWorldBounds = true;
 		barbarian.body.setOffset(89, 54);
 		barbarian.body.setSize(144, 213, false);
+		container_1.add(barbarian);
+
+		// fufuSuperDino
+		const fufuSuperDino = this.add.image(400, 235, "FufuSuperDino") as Phaser.GameObjects.Image & { body: Phaser.Physics.Arcade.Body };
+		this.physics.add.existing(fufuSuperDino, false);
+		fufuSuperDino.body.velocity.x = 100;
+		fufuSuperDino.body.velocity.y = 100;
+		fufuSuperDino.body.bounce.x = 1;
+		fufuSuperDino.body.bounce.y = 1;
+		fufuSuperDino.body.collideWorldBounds = true;
+		fufuSuperDino.body.setOffset(31, 31);
+		fufuSuperDino.body.setCircle(91);
+		container_1.add(fufuSuperDino);
+
+		// glowFx
+		fufuSuperDino.filters?.internal.addGlow(16448766, 1, 0);
 
 		// collider
-		this.physics.add.collider(fufuSuperDino, barbarian);
+		const collider = this.physics.add.collider(fufuSuperDino, barbarian);
 
 		// fufuSuperDino (components)
 		new PushOnClick(fufuSuperDino);
