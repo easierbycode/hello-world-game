81458c6a7e52e330e6e2ae311a23f45c28d7e962
▓▒░ ♔ Daniel ♔ ░▒▓ <563975+easierbycode@users.noreply.github.com>
2025-06-02 00:12:58 +0000

    launch player on gamepad down

--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -19,7 +19,12 @@ export default class Level extends Phaser.Scene {
 		/* END-USER-CTR-CODE */
 	}
 
-	editorCreate(pad: Phaser.Input.Gamepad.Gamepad): void {
+	private floor_1!: Phaser.GameObjects.Rectangle;
+	private wall!: Phaser.GameObjects.Rectangle;
+	private heroLayer!: Phaser.GameObjects.Layer;
+	private cameraFollowScript!: CameraFollowObjectScript;
+
+	editorCreate(): void {
 
 		// background
 		const background = this.add.layer();
@@ -39,44 +44,35 @@ export default class Level extends Phaser.Scene {
 		const ground = this.add.layer();
 
 		// floor_1
-		const floor_1 = this.add.rectangle(0, 569, 1280, 96);
-		floor_1.scaleY = 0.2235006528202375;
-		floor_1.setOrigin(0, 0);
-		floor_1.visible = false;
-		floor_1.isFilled = true;
-		floor_1.fillColor = 3016250;
-		floor_1.strokeColor = 0;
-		ground.add(floor_1);
+		this.floor_1 = this.add.rectangle(0, 569, 1280, 96);
+		this.floor_1.scaleY = 0.2235006528202375;
+		this.floor_1.setOrigin(0, 0);
+		this.floor_1.visible = false;
+		this.floor_1.isFilled = true;
+		this.floor_1.fillColor = 3016250;
+		this.floor_1.strokeColor = 0;
+		ground.add(this.floor_1);
 
 		// enablePhysicsBodyScript
-		new EnablePhysicsBodyScript(floor_1);
+		new EnablePhysicsBodyScript(this.floor_1);
 
 		// wall
-		const wall = this.add.rectangle(0, 150, 32, 416);
-		wall.setOrigin(0, 0);
-		wall.visible = false;
-		wall.isFilled = true;
-		wall.fillColor = 3016250;
-		wall.strokeColor = 0;
-		ground.add(wall);
+		this.wall = this.add.rectangle(0, 150, 32, 416);
+		this.wall.setOrigin(0, 0);
+		this.wall.visible = false;
+		this.wall.isFilled = true;
+		this.wall.fillColor = 3016250;
+		this.wall.strokeColor = 0;
+		ground.add(this.wall);
 
 		// enablePhysicsBodyScript_1
-		new EnablePhysicsBodyScript(wall);
+		new EnablePhysicsBodyScript(this.wall);
 
 		// elements
 		this.add.layer();
 
 		// hero
-		const hero = this.add.layer();
-
-		// barbarian
-		const barbarian = new PlayerPrefab(pad, this, 69, 400);
-		barbarian.scaleX = 0.5;
-		barbarian.scaleY = 0.5;
-		hero.add(barbarian);
-
-		// enablePhysicsBodyScript_2
-		new EnablePhysicsBodyScript(barbarian);
+		this.heroLayer = this.add.layer();
 
 		// foreground
 		this.add.layer();
@@ -85,16 +81,7 @@ export default class Level extends Phaser.Scene {
 		this.add.layer();
 
 		// cameraFollowObjectScript
-		const cameraFollowObjectScript = new CameraFollowObjectScript(this);
-
-		// collider
-		this.physics.add.collider(barbarian, floor_1);
-
-		// collider_1
-		this.physics.add.collider(barbarian, wall);
-
-		// cameraFollowObjectScript (prefab fields)
-		cameraFollowObjectScript.targetGameObject = barbarian;
+		this.cameraFollowScript = new CameraFollowObjectScript(this);
 
 		this.events.emit("scene-awake");
 	}
@@ -103,9 +90,35 @@ export default class Level extends Phaser.Scene {
 
 	// Write your code here
 
+	spawnPlayerAndFinalize(pad: Phaser.Input.Gamepad.Gamepad): void {
+
+		// barbarian
+		const barbarian = new PlayerPrefab(pad, this, 69, 400);
+		barbarian.scaleX = 0.5;
+		barbarian.scaleY = 0.5;
+		this.heroLayer.add(barbarian);
+
+		// enablePhysicsBodyScript_2
+		new EnablePhysicsBodyScript(barbarian);
+
+		// collider
+		this.physics.add.collider(barbarian, this.floor_1);
+
+		// collider_1
+		this.physics.add.collider(barbarian, this.wall);
+
+		// cameraFollowObjectScript (prefab fields)
+		this.cameraFollowScript.targetGameObject = barbarian;
+
+		// Ensure the main camera follows the barbarian
+		this.cameras.main.startFollow(barbarian);
+	}
+
 	create() {
+		this.editorCreate(); // Setup the static parts of the level first
+
 		this.scene.scene.input.gamepad?.once("down", (pad: Phaser.Input.Gamepad.Gamepad, button: Phaser.Input.Gamepad.Button) => {
-			this.editorCreate(pad);
+			this.spawnPlayerAndFinalize(pad); // Create player and related objects on gamepad input
 		});
 	}
 
